a0805d6283e8193a79bddc9ae03b53e74954471d
Ashesh Mishra <ashesh.201608@ncit.edu.np>
2025-05-06 19:49:21 +0545

    env used for DB

--- db.connection.js
+++ db.connection.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const dbName = "book";
-const dbUserName = "Ashesh";
-const dbPassword = encodeURIComponent("Hello");
-const dbHost = "school.gl3ovph.mongodb.net";
-const dbOptions = "retryWrites=true&w=majority&appName=School";
+// Load environment variables from the .env file
+dotenv.config();
 
 const connectDB = async () => {
   try {
+    // Access environment variables using process.env
+    const dbName = process.env.DB_NAME;
+    const dbUserName = encodeURIComponent(process.env.DB_USERNAME);
+    const dbPassword = encodeURIComponent(process.env.DB_PASSWORD);
+    const dbHost = process.env.DB_HOST;
+    const dbOptions = process.env.DB_OPTIONS;
+
+    // Create the MongoDB connection URL
     const url = `mongodb+srv://${dbUserName}:${dbPassword}@${dbHost}/${dbName}?${dbOptions}`;
 
+    // Connect to MongoDB
     await mongoose.connect(url);
 
     console.log("DB connection established...");
